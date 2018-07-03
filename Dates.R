64784f9c0d57826f82c940c320a124a222bba0d4
Pgalante <pgalante@amnh.org>
2018-07-03 14:52:09 -0400

    unique tibbles

--- Dates.R
+++ Dates.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(raster)
 ##############Global Wc to crop 
-datedRasterStack<-stack(list.files(path = '/home/pjg/worldClim/wc2-5', pattern = '\\.tif$', full.names = T))
+datedRasterStack<-stack(list.files(path = '/home/pgalante/Projects/rasterData/wc2-5', pattern = '\\.tif$', full.names = T))
 ## Give fake dates  -- for now using 2 years because thats what I have occ data for
 years<-c(rep("2008", 10), rep("2009", 10))
 for (i in 1:19){
@@ -9,12 +9,20 @@ for (i in 1:19){
 }
 
 #### Load and recognize Dates from files in R
-dtab1<-read_csv('/home/pjg/Git/WorkingCode/TestData.csv')
+dtab1<-read_csv('/home/pgalante/Projects/Git/WorkingCode/TestData.csv') 
 colnames(dtab1)<-c('x','y','date')
+reDate <- function(dtab1){
+  dtab1 %>% dplyr::select(x,y,date) %>%
+  mutate(date = mdy_hm(date)) %>%
+  mutate(years = year(date)) %>% 
+  mutate(months = month(date)) %>%
+  mutate(days = day(date))
+}
+dtab2<-reDate(dtab1)
+
 ## Crop global Wc to smaller area 
 e<-extent(dtab1)
 datedRasterStack<-crop(datedRasterStack, e)            
-
 
 ###########
 ## Pair occs with corresponding env 
@@ -25,17 +33,15 @@ library(lubridate)
 dateScale<-"year"
 occDates<-function(dtab1, dateScale){
   require(lubridate)
-  d<-lapply(dtab1$date, mdy_hm)
-  d.df <- tbl_df(cbind(lapply(d, year), lapply(d, month), lapply(d, day)))
-  colnames(d.df) <- c("year", "month", "day")
+  
   if (dateScale == "year"){
-    return(unique(dplyr::select(d.df, year)))
+    return(unique(dplyr::select(dtab2, years)))
   } 
   if (dateScale == "month"){
-    return(unique(dplyr::select(d.df, year, month)))
+    return(unique(dplyr::select(dtab2, years, months)))
   } 
   if (dateScale == "day"){
-    return(unique(dplyr::select(d.df, year, month, day)))
+    return(unique(dplyr::select(dtab2, years, months, days)))
   }
 }
 uniDates<-occDates(dtab1, dateScale = "year")
@@ -63,23 +69,39 @@ class(uniDates)
 
 
 # Make a list of rasterStacks for each unique year
-rasNames <- lapply(test[[1]], function(x) datedRasterStack[[grep(x, names(datedRasterStack))]])
+rasNames <- lapply(uniDates[[1]], function(x) datedRasterStack[[grep(x, names(datedRasterStack))]])
 # Create different tibbles for each unique dateScale
 subOccs <- function(dateScale, dtab1, uniDates){
   if (dateScale == "year"){
-    dtab2 <- dtab1 %>% mutate(date = mdy_hm(date)) %>% 
+    t1<-NULL    
+    for (i in 1:length(unique(dtab2$years))){
+      t1[[i]] = filter(dtab2, years == as.list(unique(dtab2$years))[[i]])
+    }}
+  #### NEED TO FIGURE OUT YEAR MONTH DAY COMBINATIONS
+  if (dateScale == "month"){
+    t1<-NULL    
+    for (i in 1:length(unique(dtab2$years))){
+      t1[[i]] = filter(dtab2, months == as.list(unique(dtab2$months))[[i]])
+    }}
+  if (dateScale == "day"){
+    t1<-NULL    
+    for (i in 1:length(unique(dtab2$days))){
+      t1[[i]] = filter(dtab2, days == as.list(unique(dtab2$days))[[i]])
+    }}
+}
+
+
+dtab2 %>% lapply(uniDates, function(x) filter(dtab2, year(dtab2$date)==uniDates[[1]][x]))
+      class(unique(dtab2$years)[1])
       
-lapply(a, function(x) filter(dtab2, year(date) == a))
-    
+lapply(uniDates, function(x) filter(dtab2, year(date) == uniDates))
+
+t1<-NULL    
 for (i in 1:2){
-  t1[[i]]<-filter(dtab2, year(dtab2$date)==uniques[[1]][i])
+  t1[[i]]<-filter(dtab2, year(dtab2$date)==uniDates[[1]][i])
 }    
 
 lapply(uniDates[[1]], function (x) filter(year(dtab2$date) == x))
-
-
-    
-dtab2 %>% distinct(year(date))
 
 distinct(year(mdy_hm(dtab1$date)))
     
